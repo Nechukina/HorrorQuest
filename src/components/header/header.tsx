a869a7f12147f280a337d5bd165bcf11b8b73d77
Ksenia <ksenechka.93@gmail.com>
2023-05-15 21:18:12 +0300

    add: links in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function Header(): JSX.Element {
   return (
     <header className="header">
       <div className="container container--size-l">
         <span className="logo header__logo">
-          <svg width="134" height="52" aria-hidden="true">
-            <use xlinkHref="#logo"></use>
-          </svg>
+          <Link className="link" to={AppRoute.Main}>
+            <svg width="134" height="52" aria-hidden="true">
+              <use xlinkHref="#logo"></use>
+            </svg>
+          </Link>
         </span>
         <nav className="main-nav header__main-nav">
           <ul className="main-nav__list">
@@ -13,7 +18,7 @@ function Header(): JSX.Element {
               <a className="link active" href="index.html">Квесты</a>
             </li>
             <li className="main-nav__item">
-              <a className="link" href="contacts.html">Контакты</a>
+              <Link className="link" to={AppRoute.Contacts}>Контакты</Link>
             </li>
             <li className="main-nav__item">
               <a className="link" href="my-quests.html">Мои бронирования</a>
@@ -21,7 +26,7 @@ function Header(): JSX.Element {
           </ul>
         </nav>
         <div className="header__side-nav">
-          <a className="btn btn--accent header__side-item" href="#">Выйти</a>
+          <Link className="btn btn--accent header__side-item" to={AppRoute.Login}>Выйти</Link>
           <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
         </div>
       </div>
--- src/pages/page-404/page-404.tsx
+++ src/pages/page-404/page-404.tsx
@@ -36,7 +36,7 @@ function Page404(): JSX.Element {
             <div className="quest-page__content">
               <h1 className="title title--size-l title--uppercase quest-page__title">Page 404</h1>
 
-              <p className="quest-page__description">Похоже, Вы попали на несуществующую страницу. Попробуйте ещё раз...</p>
+              <p className="quest-page__description">Похоже, Вы попали на несуществующую страницу. Попробуйте вернуться на главную...</p>
               <Link className="btn btn--accent btn--cta quest-page__btn" to={AppRoute.Main}>Назад на главную</Link>
             </div>
           </div>
